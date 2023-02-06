fbc6f8e5c76c27b5c24bb7516a5a273f8901d828
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-02-06 23:36:04 +0100

    Delete hellobaby.py

--- hellobaby.py
+++
@@ -1,8 +0,0 @@
-import jinja2
-
-name = input('please enter your name here \n')
-
-env = jinja2.Environment()
-
-template = env.from_string("Hello, baby {{ name }}!")
-print(template.render(name= name))
