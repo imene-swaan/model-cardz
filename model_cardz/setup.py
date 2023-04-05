9d3be448c8e49b5c326be9f077e44c70e1e5c431
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-04-05 09:57:36 +0200

    Update setup.py

--- model_cardz/setup.py
+++ model_cardz/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cardz',
-      version='0.1',
+      version='0.1.0',
       description='Model Cards Generator',
       packages=['cardz'],
       zip_safe=False)
